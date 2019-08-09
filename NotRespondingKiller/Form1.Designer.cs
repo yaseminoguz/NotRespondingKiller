8602455da1d2e63728efaddf57c0ecf0d7a1f83c
Yasemin Oğuz <34984064+yaseminoguz@users.noreply.github.com>
2019-08-09 14:42:00 +0300

    Add files via upload

---
+++ NotRespondingKiller/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace NotRespondingKiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(524, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(111, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(524, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Program";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(33, 63);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(42, 20);
+            this.Label2.TabIndex = 3;
+            this.Label2.Text = "Path";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(650, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(37, 105);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(688, 223);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Kontrol P. Sn :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 400);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Bekleme Sn :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(148, 354);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(60, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(148, 397);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(60, 26);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 34);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(37, 468);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(687, 241);
+            this.listView2.TabIndex = 12;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(650, 340);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 37);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 735);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ NotRespondingKiller/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Timers;
+using System.IO;
+
+namespace NotRespondingKiller
+{
+    public partial class Form1 : Form
+    {
+        string name;
+        string path;
+        int kontrol_sn = 3000;
+        int bekleme_sn = 1000;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void timer_start()
+        {
+            timer1.Interval = kontrol_sn;
+            timer1.Enabled = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.Columns.Add("Programın Adı", 150);
+            listView1.Columns.Add("Path", 300);
+            listView2.View = View.Details;
+            listView2.FullRowSelect = true;
+            listView2.Columns.Add("Name", 150);
+            listView2.Columns.Add("Path", 150);
+            listView2.Columns.Add("Time", 150);
+            timer_start();            
+        }
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string[] arr = new string[4];
+            ListViewItem itm;
+            name = textBox1.Text;
+            path = textBox2.Text+ "\\"+ name;
+            arr[0] = name;
+            arr[1] =  path;            
+            itm = new ListViewItem(arr);
+            listView1.Items.Add(itm);
+            StreamWriter sw =File.AppendText("C:\\Users\\Matriks\\Desktop\\kayıt.txt");
+            sw.WriteLine(arr[0] + " "+arr[1] + " Kontrol mSn: "+ kontrol_sn+" Bekleme mS "+bekleme_sn);            
+            sw.Flush();
+            sw.Close();           
+        }
+        
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+           
+            List<Process> prs= GetProcessByFilename(path);                               
+            string[] arr = new string[3];
+            foreach (Process pr in prs)
+            {
+                ListViewItem itm;
+                itm = new ListViewItem(pr.ProcessName.ToLower());                              
+                if (!pr.Responding)
+                {
+                    pr.Kill();                  
+                    arr[0] = name;
+                    arr[1] = path;
+                    arr[2] = DateTime.Now.ToString()+"kapandı.";                                        
+                    itm = new ListViewItem(arr);
+                    listView2.Items.Add(itm);               
+                    islem(DateTime.Now);
+                    timer1.Stop();
+                    Thread.Sleep(bekleme_sn);
+                    start();
+                    arr[0] = name;
+                    arr[1] = path;
+                    arr[2] = DateTime.Now.ToString() + "açıldı.";
+                    itm = new ListViewItem(arr);
+                    listView2.Items.Add(itm);
+                    timer1.Start();                
+                }
+            }
+        }
+        void start()
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = name;
+            //startInfo.Arguments = file;
+            Process.Start(startInfo);
+        }
+        private static List<Process> GetProcessByFilename(string filename)
+        {
+            List<Process> processes = new List<Process>();
+            processes= Process.GetProcesses().Where(process => !string.IsNullOrEmpty(process.MainWindowTitle) &&
+                process.MainModule.FileName == filename).ToList();
+           
+            return processes;
+        }
+        void islem(DateTime aa)
+        {
+            string[] arr = new string[4];
+            ListViewItem itm;
+            arr[0] = name;
+            arr[1] = DateTime.Now.ToString();
+            itm = new ListViewItem(arr);
+            listView2.Items.Add(itm);
+        }    
+        private void Button2_Click(object sender, EventArgs e)
+        {           
+            if (textBox3.Text != "")
+            {
+                kontrol_sn = Convert.ToInt32(textBox3.Text)*1000;
+            }
+            if (textBox4.Text != "")
+            {
+                bekleme_sn = Convert.ToInt32(textBox4.Text)*1000;
+            }
+            timer_start();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem item in listView1.SelectedItems)
+            {
+                item.Remove();
+            }
+        }
+    }
+}
